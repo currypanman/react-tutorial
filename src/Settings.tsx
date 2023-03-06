73b8c5fc892df76a0428b61a0a47e89e5b0d011a
Currypanman <currypanman.spicy@outlook.com>
2023-03-07 00:05:26 +0900

    Split index.tsx.

---
+++ src/Settings.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type SettingsProp = {
+};
+
+class Settings extends React.Component<SettingsProp> {
+  render () {
+    return (
+      <div>
+        This game is not configurable (^_^).
+      </div>
+    );
+  }
+}
+
+export { Settings };
---
+++ src/Square.tsx
@@ -0,0 +1,14 @@
+type SquareProps = {
+  value: string,
+  onClick: () => void,
+};
+  
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+export { Square };
